aa04c239d410613160846271346dc6b371b9fa7d
kalman61 <1372766632@qq.com>
2019-07-25 16:11:17 +0800

    add all hello file

---
+++ hello/test.py
@@ -0,0 +1,9 @@
+# phone_number = '1386-666-0006'
+# hiding_number = phone_number.replace(phone_number[:9],'-' * 9)
+# print(hiding_number)
+#A first Python script 
+import sys 
+print(sys. platform)
+print(2**100) 
+x=' Spam!'
+print(x*8)
